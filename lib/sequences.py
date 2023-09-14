1e2f62835bfb9994a26baaa9ea3e721c9d0257cf
Nial Johnson <nialj707@gmail.com>
2023-09-14 09:38:07 -0700

    Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    seq = []
+    if length > 0:
+        seq.append(0)
+    if length >= 2:
+        seq.append(1)
+        for i in range(2, length):
+            seq.append(seq[-1] + seq[-2])
+    print(seq)
